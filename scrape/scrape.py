ea3fae03cdb69c521b8dfc820b563a84bc2a4934
Michal Borek <63055456+michalaborek@users.noreply.github.com>
2020-05-22 18:09:43 +0200

    export to json started

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 """This part is for developing/testing offline"""
 with open('scrape/movies.html', "r", encoding="utf-8") as file:
@@ -14,6 +15,33 @@ class Scrape:
     # TV_SOURCE = 'https://www.filmweb.pl/ranking/vod/netflix/serial'
     MOVIES_SOURCE = movies_html
     TV_SOURCE = tv_series_html
+
+    def export_json(self, json_content, req_type):
+        """This method saves json data to file"""
+
+        path = ''
+        current_date = ''
+
+        if req_type.upper() == 'TV':
+            with open('tv_series.json', 'w') as outfile:
+                json.dump(json_content, outfile)
+
+        if req_type.upper() == 'MOVIE':
+            with open('movies.json', 'w') as outfile:
+                json.dump(json_content, outfile)
+
+    def get_data_to_json(self, req_type: str = 'all'):
+        """Export json file for tv series, films or both by default"""
+
+        if req_type.upper() == 'TV' or req_type.upper() == 'MOVIE':
+            json_content = self.get_ranks_list(req_type)
+            self.export_json(json_content, req_type)
+
+        else:
+            for req_type in ['TV', 'MOVIE']:
+                json_content = self.get_ranks_list(req_type)
+                self.export_json(json_content, req_type)
+
 
     def get_ranks_list(self, req_type: str):
         """This is the main method - it returns list of dictionaries
@@ -52,10 +80,13 @@ class Scrape:
         if movie_in.find('div', class_='film__original'):
             movie_data['original_title'] = movie_in.find('div', class_='film__original').text.strip()
         movie_data['year'] = movie_in.find(class_='film__production-year').text.strip().replace(')', '').replace('(', '')
+        movie_data['rating'] = movie_in.find(class_='rate__value').text
+        raw_rate_count = movie_in.find(class_='rate__count').text
+        clean_rate_count = raw_rate_count.replace('oceny','').replace('ocen','').strip().replace(' ','')
+        movie_data['rate_count'] = clean_rate_count
         return movie_data
 
 
 scraper = Scrape()
-result = scraper.get_ranks_list('movie')
+scraper.get_data_to_json('movie')
 
-print(result)
